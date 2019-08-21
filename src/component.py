d5fa60d3b5ae9b61bddb8af2148c316c6523302e
Leo Chan <cleojanten@hotmail.com>
2019-08-21 11:56:06 -0700

    gelf enabled

--- src/component.py
+++ src/component.py
@@ -4,6 +4,9 @@ Template Component main class.
 '''
 
 import logging
+import logging_gelf.handlers
+import logging_gelf.formatters
+import os
 import sys
 import json
 from datetime import datetime  # noqa
@@ -35,13 +38,33 @@ MANDATORY_IMAGE_PARS = []
 # Default Table Output Destination
 DEFAULT_TABLE_SOURCE = "/data/in/tables/"
 
-APP_VERSION = '0.0.1'
+# Logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)-8s : [line:%(lineno)3s] %(message)s',
+    datefmt="%Y-%m-%d %H:%M:%S")
+
+if 'KBC_LOGGER_ADDR' in os.environ and 'KBC_LOGGER_PORT' in os.environ:
+
+    logger = logging.getLogger()
+    logging_gelf_handler = logging_gelf.handlers.GELFTCPSocketHandler(
+        host=os.getenv('KBC_LOGGER_ADDR'), port=int(os.getenv('KBC_LOGGER_PORT')))
+    logging_gelf_handler.setFormatter(
+        logging_gelf.formatters.GELFFormatter(null_character=True))
+    logger.addHandler(logging_gelf_handler)
+
+    # remove default logging to stdout
+    logger.removeHandler(logger.handlers[0])
+
+
+APP_VERSION = '0.0.3'
 
 
 class Component(KBCEnvHandler):
 
     def __init__(self, debug=False):
         KBCEnvHandler.__init__(self, MANDATORY_PARS)
+        """
         # override debug from config
         if self.cfg_params.get('debug'):
             debug = True
@@ -49,6 +72,7 @@ class Component(KBCEnvHandler):
             debug = False
 
         self.set_default_logger('DEBUG' if debug else 'INFO')
+        """
         logging.info('Running version %s', APP_VERSION)
         logging.info('Loading configuration...')
 
